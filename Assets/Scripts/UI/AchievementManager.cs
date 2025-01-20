f3cfa71b8b5909b141fbbdd3bba49bd18d14c5e5
Lozmep <lozmep.adm@gmail.com>
2025-01-20 18:30:48 -0500

    Add changes to scene

--- Assets/Scripts/UI/AchievementManager.cs
+++ Assets/Scripts/UI/AchievementManager.cs
@@ -25,8 +25,6 @@ public class AchievementManager : MonoBehaviour
         if (Mathf.FloorToInt(elapsedTime) > 10)
         {
             Debug.Log("Han pasado 10 segundos.");
-            achievementSystem.CompareValuesInChildren(0);
-            achievementSystem.CompareValuesInChildren(1);
             achievementSystem.CompareValuesInChildren(2);
             stopExecution = true;
             gameObject.SetActive(false);
