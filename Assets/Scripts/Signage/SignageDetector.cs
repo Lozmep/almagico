f74b9421039faf562660f2f309c7cea8ef94f43b
Valuv <valuv027@gmail.com>
2025-01-22 17:05:50 -0800

    Puros debugs

--- Assets/Scripts/Signage/SignageDetector.cs
+++ Assets/Scripts/Signage/SignageDetector.cs
@@ -1,10 +1,12 @@
 using DialogueSystem;
 using SignalSystem;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class SignageDetector : MonoBehaviour
 {
-    public float radius; 
+   
     public string playerTag = "Player"; 
     public GameObject signObject;
     public SignalDialogue signalDialogue;
@@ -12,23 +14,35 @@ public class SignageDetector : MonoBehaviour
     public TakeItemEvent takeItem;
     public string type;
 
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+    [SerializeField] private TextMeshProUGUI textType;
+    [SerializeField] private TextMeshProUGUI textSignal;
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag(playerTag))
         {
             signObject.SetActive(true);
+            string dialogueManagerBool = dialogueManager.isActive.ToString();
+            string takeItemBoolean = takeItem.isFree.ToString();
+            string getKeyBoolean = Input.GetKey(KeyCode.X).ToString();
+
+            textMeshPro.text = "El dialogo esta " + dialogueManagerBool + ", las manos estan " + takeItemBoolean + ", la tecla esta " + getKeyBoolean;
 
             if (Input.GetKey(KeyCode.X) && !dialogueManager.isActive && takeItem.isFree)
             {
+                textType.text = type;
                 SignalObject signal = null;
                 foreach (var signalObject in signalDialogue.signalPool)
                 {
+                    textSignal.text = signalObject.type.ToString();
                     if (signalObject.type == type)
                     {
                         signal = signalObject;
                         break;
                     }
                 }
+                
                 if (signal != null)
                 {
                     StartCoroutine(dialogueManager.Speak(signal.dialogue.spanish));
